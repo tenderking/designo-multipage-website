252a97f9a809ab3c2fba9c765310176cbc14fba5
tenderking <gmu003@gmail.com>
2023-04-10 23:40:06 +0200

    create nav component

--- app.vue
+++ app.vue
@@ -1,5 +1,53 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <TheNavBar />
   </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap');
+:root{ 
+	--color-primary-peach: hsla(11,72.1%,66.3%,1);
+	--color-primary-black: hsla(270,3.4%,11.4%,1);
+	--color-primary-white: hsla(0,0%,100%,1);
+	--color-secondary-peach: hsla(11,100%,80.4%,1);
+	--color-secondary-dark-grey: hsla(264,4.9%,20.2%,1);
+	--color-secondary-light-grey: hsla(210,16.7%,95.3%,0.858);
+}
+
+#app {
+	font-size: 16px;
+
+	font-family: "Jost";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 26px;
+	text-decoration: none;
+	text-transform: none;
+}
+li,a{
+	font-family: "Jost";
+	font-weight: 500;
+	letter-spacing: 5px;
+	text-transform: uppercase;
+}
+h1 {
+	font-size: 48px;
+	font-weight: 500;
+	line-height: 48px;
+}
+h2 {
+	font-size: 40px;
+	font-family: "Jost";
+	font-weight: 500;
+	line-height: 48px;
+	letter-spacing: 2px;
+}
+h3 {
+	font-size: 20px;
+	font-family: "Jost";
+	font-weight: 500;
+	line-height: 26px;
+	letter-spacing: 5px;
+	text-transform: uppercase;
+}
+</style>
---
+++ components/TheNavBar.vue
@@ -0,0 +1,92 @@
+
+<script setup lang="ts">
+ //use vue-use to toggle menu-toggle
+
+</script>
+<template>
+  <nav>
+      <NuxLink to="/">
+          <img src="/assets/shared/desktop/logo-dark.png" class="logo-dark" />
+      </NuxLink>
+      <div class="menu-toggle" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul class="menu" :class="{ 'active': isMenuOpen }">
+        <li>
+          <NuxLink to="/">Our Company</NuxLink>
+        </li>
+        <li>
+          <NuxLink to="/about">Locations</NuxLink>
+        </li>
+        <li>
+          <NuxLink to="/contact">Contact</NuxLink>
+        </li>
+      </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav{
+ display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo-dark{
+width:202px;
+}
+.menu-toggle {
+  display: none;
+  cursor: pointer;
+}
+
+.menu-toggle span {
+  display: block;
+  height: 2px;
+  width: 25px;
+  margin-bottom: 5px;
+  background-color: #333;
+}
+
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu li {
+  margin-left: 20px;
+}
+@media screen and (max-width: 768px) {
+  .menu {
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  }
+
+  .menu.active {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .menu li {
+    margin: 10px 0;
+  }
+
+  .menu-toggle {
+    display: block;
+  }
+}
+</style>
+
+
